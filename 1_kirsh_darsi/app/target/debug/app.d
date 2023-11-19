2927fe7f0c866baeb0171a1cde2b338e4f83d166
Ju8Sho <ju8sho@gmail.com>
2023-11-19 20:36:31 +0500

    rust asoslar

---
+++ 1_kirsh_darsi/app/target/debug/app.d
@@ -0,0 +1,1 @@
+/home/jusho/Документы/GitHub/RustLession/1_kirsh_darsi/app/target/debug/app: /home/jusho/Документы/GitHub/RustLession/1_kirsh_darsi/app/src/main.rs
---
+++ 1_kirsh_darsi/app/target/debug/deps/app-ad4e43fe46f609d3.d
@@ -0,0 +1,5 @@
+/home/jusho/Документы/GitHub/RustLession/1_kirsh_darsi/app/target/debug/deps/app-ad4e43fe46f609d3: src/main.rs
+
+/home/jusho/Документы/GitHub/RustLession/1_kirsh_darsi/app/target/debug/deps/app-ad4e43fe46f609d3.d: src/main.rs
+
+src/main.rs:
